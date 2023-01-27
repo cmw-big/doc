d09bfaa35c28fe2d291a2f766d2f7132d9e790cb
pingfa <pingfa@pinduoduo.com>
2023-01-27 14:11:31 +0800

    feat: 修复

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,8 +1,11 @@
-import {type FC} from 'react'
+import { type FC } from 'react';
 export interface ButtonProps {
-
+  /**
+   * 我表示类型
+   */
+  type?: string;
 }
-const Button:FC<ButtonProps> = (props)=>{
-  return <button style={{fontSize: 20}}>我是button</button>
-}
-export default Button
+const Button: FC<ButtonProps> = (props) => {
+  return <button style={{ fontSize: 20 }}>我是button</button>;
+};
+export default Button;
