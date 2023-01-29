feb096234c27d8acd41b7e2d89fc37c5055db971
pingfa <pingfa@pinduoduo.com>
2023-01-29 16:53:50 +0800

    feat: 修复问题

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -10,26 +10,3 @@ const Button: FC<ButtonProps> = (props) => {
 };
 export default Button;
 
-type Colors = 'red' | 'green' | 'blue';
-
-type RGB = [red: number, green: number, blue: number];
-type o = Partial<Record<Colors, string | RGB>>;
-
-enum T {
-  a = 'a',
-  b = 'b',
-}
-type f = Partial<Record<T, number | string>>;
-const tt: f = {
-  [T.a]: 1,
-} satisfies f;
-
-const palette = {
-  red: [255, 0, 0],
-  green: '#00ff00',
-  blue: [0, 0, 255],
-} satisfies o;
-
-// 依然可以访问这些方法
-const redComponent = palette.red.map;
-const greenNormalized = palette.green.toUpperCase();
